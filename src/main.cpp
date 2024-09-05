62e5915debf7bccb4dc4c4c6c9a2cebc0910bc10
leohong0814 <leohong0814@gmail.com>
2024-09-05 22:29:58 +0800

    [FIX] Change code framework to RTOS

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <Arduino_FreeRTOS.h>
 
 #define StartPin 0
 #define ExtendPin 1
@@ -14,14 +15,26 @@ const int ExtenInterval = 90;
 int CurrInterval = CountDownInterval;
 int CurrTime = 0;
 
+TaskHandle_t CountDownTaskHandle;
+TaskHandle_t BuzzerTaskHandlel;
+
+void CountDownTask(void *pvParameters);
+void BuzzerTask(void *pvParameters);
+
 enum Situation
 {
   Pause,
   Running,
   Extend,
 };
-
+enum BuzzerType
+{
+  None,
+  Hint,
+  Alert,
+};
 enum Situation Status = Pause;
+enum BuzzerType Buzzer = None;
 
 const byte digitToSegments[] = {
     0b00111111, // 0
@@ -40,25 +53,19 @@ void handleStartPin()
 {
   CurrTime = 0;
   Status = Running;
+  Buzzer = None;
 }
 
 void handleExtendPin()
 {
-  CurrTime = 0;
   Status = Extend;
+  Buzzer = None;
 }
 
 void handlePausePin()
 {
-  CurrTime = 0;
   Status = Pause;
-}
-
-void alartReachThreshold()
-{
-  digitalWrite(BuzzerPin, HIGH);
-  delay(1500);
-  digitalWrite(BuzzerPin, LOW);
+  Buzzer = None;
 }
 
 void lightDigit(int digit, int value)
@@ -88,37 +95,6 @@ void displayNumber(int num)
   }
 }
 
-void showCountDown()
-{
-  switch (Status)
-  {
-  case Pause:
-    return;
-    break;
-  case Running:
-    CurrInterval = CountDownInterval;
-    break;
-  case Extend:
-    CurrInterval = ExtenInterval;
-    break;
-  default:
-    break;
-  }
-  displayNumber(CurrInterval - CurrTime);
-  delay(1000);
-  CurrTime = CurrTime + 1;
-  if (CurrInterval - CurrTime == 5)
-  {
-    alartReachThreshold();
-  }
-  else if (CurrInterval == CurrTime)
-  {
-    CurrTime = 0;
-    alartReachThreshold();
-    Status = Pause;
-  }
-}
-
 void setup()
 {
   pinMode(StartPin, INPUT);
@@ -137,9 +113,78 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(StartPin), handleStartPin, FALLING);
   attachInterrupt(digitalPinToInterrupt(ExtendPin), handleExtendPin, FALLING);
   attachInterrupt(digitalPinToInterrupt(PausePin), handlePausePin, FALLING);
+  xTaskCreate(CountDownTask, "CountDownrTask", 128, NULL, 1, &CountDownTaskHandle);
+  xTaskCreate(BuzzerTask, "BuzzerTask", 128, NULL, 1, &BuzzerTaskHandlel);
+  vTaskStartScheduler();
 }
 
 void loop()
 {
-  showCountDown();
+  
+}
+
+void CountDownTask(void *pvParameters) {
+  (void) pvParameters;
+
+  while (true)
+  {
+    switch (Status)
+    {
+      case Pause:
+      CurrTime = 0;
+      Buzzer = None;
+        continue;
+        break;
+      case Running:
+        CurrInterval = CountDownInterval;
+        break;
+      case Extend:
+        CurrInterval = ExtenInterval;
+        break;
+      default:
+        break;
+    }
+    displayNumber(CurrInterval - CurrTime);
+    vTaskDelay(1000 / portTICK_PERIOD_MS); 
+    CurrTime = CurrTime + 1;
+    if (CurrInterval - CurrTime == 5)
+    {
+      Buzzer = Hint;
+    }
+    else if (CurrInterval == CurrTime)
+    {
+      CurrTime = 0;
+      Buzzer = Alert;
+      Status = Pause;
+    }
+  }
+}
+
+void BuzzerTask(void *pvParameters) {
+  (void) pvParameters;
+
+  while (true)
+  {
+    switch (Buzzer)
+    {
+    case None:
+    default:   
+      break;
+    case Hint:
+      digitalWrite(BuzzerPin, HIGH);
+      vTaskDelay(500 / portTICK_PERIOD_MS);
+      digitalWrite(BuzzerPin, LOW);
+      vTaskDelay(500 / portTICK_PERIOD_MS);
+      digitalWrite(BuzzerPin, HIGH);
+      vTaskDelay(500 / portTICK_PERIOD_MS);
+      digitalWrite(BuzzerPin, LOW);
+      Buzzer = None;
+      break;
+    case Alert:
+      digitalWrite(BuzzerPin, HIGH);
+      vTaskDelay(2500 / portTICK_PERIOD_MS); 
+      Buzzer = None;
+      break;
+    } 
+  }
 }
